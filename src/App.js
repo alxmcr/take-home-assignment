166ecab247789fe2e5cd877401ac9661bed951ef
Alejandro M. Coca <alxmcr@outlook.com>
2021-03-10 17:54:27 -0400

    Format paragraphs by rules and fix details separate paragraphs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 import React from "react";
 
 const EMPTY = "";
+const SPACE = " ";
+const LINE_BREAK = "\n";
 const DOUBLE_LINE_BREAK = "\n\n";
+const MAX_LENGTH_BY_LINE = 80;
 
 function App() {
   const [textInput, setTextInput] = React.useState(`This is
@@ -21,30 +24,51 @@ This      is a second paragraph with extraneous whitespace.`);
   };
 
   const transformText = input => {
-    let output = input;
-    /*
-        if input.length <= 1,
-        it means it has only character or is an empty string and
-        it's not necessary to format something
-    */
-    if (input.length > 1) {
-      output = "";
-      const paragraphs = input.split(DOUBLE_LINE_BREAK);
-      const paragraphsValid = paragraphs.filter(paragraph => paragraph !== EMPTY)
+    let output = "";
+    const paragraphsFormatted = []
+    const paragraphs = input.split(DOUBLE_LINE_BREAK);
+    const paragraphsValid = paragraphs.filter(paragraph => paragraph !== EMPTY)
 
-      const paragraphsFormatted = []
+    paragraphsValid.forEach(paragraph => {
+      const paragraphFormatted = transformParagraph(paragraph);
+      paragraphsFormatted.push(paragraphFormatted);
+    })
 
-      paragraphsValid.forEach(paragraph => {
-        const paragraphFormatted = transformParagraph(paragraph);
-        paragraphsFormatted.push(paragraphFormatted);
-      })
+    if (paragraphsFormatted.length > 1) {
       output = paragraphsFormatted.join(DOUBLE_LINE_BREAK);
+    } else {
+      output = paragraphsFormatted;
     }
     setTextOutput(output);
   }
 
   const transformParagraph = (paragraph = "") => {
-    let paragraphFormatted = paragraph;
+    let paragraphFormatted = "";
+    const paragraphReplaceLineBreak = paragraph.replaceAll(LINE_BREAK, SPACE);
+    const words = paragraphReplaceLineBreak.split(SPACE);
+    const wordsValid = words.filter(word => word !== EMPTY)
+    let line = "";
+    wordsValid.forEach((word) => {
+      const currentLineLength = line.length;
+      const wordLength = word.length;
+      const lineLength = currentLineLength + wordLength;
+
+      if (lineLength <= MAX_LENGTH_BY_LINE) {
+        if (line === "") {
+          line = word;
+        } else {
+          line = line + " " + word;
+        }
+      } else {
+        paragraphFormatted = paragraphFormatted + line;
+        if (line === "") {
+          line = word;
+        } else {
+          line = "\n" + word;
+        }
+      }
+    })
+    paragraphFormatted = paragraphFormatted + line;
     return paragraphFormatted;
   }
 
